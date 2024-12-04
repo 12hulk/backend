8e5d02aa9145d2532b40b95c90ef60d96e4a77ad
Dilpreet <dilpreetd88@gmail.com>
2024-12-04 13:07:07 -0600

    5

--- api/register.js
+++ api/register.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const pool = createClient(
+const supabase = createClient(
     "https://ekdoxzpypavhtoklntqv.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZG94enB5cGF2aHRva2xudHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNzQ3NDAsImV4cCI6MjA0ODY1MDc0MH0.FyHH1ee-dfBThvAUeL4SaqCO6sJZzQ-2Scnnv-bInOA"
 );
