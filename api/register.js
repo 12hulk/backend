17858bd7e824a5e6ac94abe365f093eb7b70c24e
Dilpreet <dilpreetd88@gmail.com>
2024-12-04 13:05:04 -0600

    5

--- api/register.js
+++ api/register.js
@@ -5,7 +5,7 @@ const pool = createClient(
     "https://ekdoxzpypavhtoklntqv.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZG94enB5cGF2aHRva2xudHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNzQ3NDAsImV4cCI6MjA0ODY1MDc0MH0.FyHH1ee-dfBThvAUeL4SaqCO6sJZzQ-2Scnnv-bInOA"
 );
-import bcrypt from "bcryptjs";
+
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -33,12 +33,12 @@ export default async function handler(req, res) {
 
             // Hash the password
 
-            const hashedPassword = await bcrypt.hash(password, 10);
+
 
             // Insert the new user
             const { data: newUser, error: insertError } = await supabase
                 .from('users')
-                .insert([{ name, email, password_hash: hashedPassword }])
+                .insert([{ name, email, password_hash: password }])
                 .select()
                 .single();
 
