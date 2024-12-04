ddad016db72838341b0d5150dfa074585ce81959
Dilpreet <dilpreetd88@gmail.com>
2024-12-04 13:58:54 -0600

    5

--- api/register.js
+++ api/register.js
@@ -11,7 +11,11 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://file-sharing-website-vuzt.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        // Respond with a status 200 OK for the preflight request
+        return res.status(200).end();
+    }
     if (req.method === 'POST') {
         const { name, email, password } = req.body;
 
