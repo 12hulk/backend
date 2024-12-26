5d1e9611e6823ba18191cdd11f81d64854fe274a
Dilpreet <dilpreetd88@gmail.com>
2024-12-26 11:55:47 -0600

    final

--- api/register.js
+++ api/register.js
@@ -12,6 +12,11 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     // Handle preflight request
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        // Respond with a status 200 OK for the preflight request
+        return res.status(200).end();
+    }
 
     if (req.method === "POST") {
         const { name, email, password } = req.body;
